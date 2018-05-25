39f0c5d46109febd15ddc37bf3045865b6a9bf7e
wheeemyse <wheemyse>
2018-05-25 10:20:26 +0200

    gfdgd

--- Exoparis/formulaire.php
+++ Exoparis/formulaire.php
@@ -8,60 +8,26 @@
 
 
 <?php
-        if (isset ($_POST["sexe"]) && isset ($_POST["prenom"]) && isset($_POST["nom"])) {
         $sexe = $_POST["sexe"];
-        $prenom = (string) $_POST ["prenom"];
-        $nom = (string) $_POST ["nom"];
-        $email = (string) $_POST ["mail"];
-        $remember = $_POST ["remember"];
-      }
+        $prenom =  $_POST["prenom"];
+        $nom = $_POST["nom"];
+        $email = $_POST["mail"];
 
-
-    if (isset($remember)) {
+    if (isset($_POST["remember"])) {
      $remember = "se souvenir de moi";
     }
 
     else {
       $remember = 'ne pas se souvenir de moi';
     }
-?>
-        $message = <table>
-          <tr>
-             <th><label>Titre:<span class="ast">*</span></label></th>
-             <th>'<p> . $sexe . '</p></tr>
-          <tr>
-            <td><label>Prénom:<span class="ast">*</span></label></td>
-            <td> <p>' .  $prenom . '</p> </tr>
-          <tr>
-            <td><label>Nom:<span class="ast">*</span></label></td>
-            <td> <p>' . $nom . '</p> </td>
-          </tr>
-          <tr>
-            <td>email:</td>
-            <td> <p>' . $email . '</p> </tr>
-        </table>
 
-<?php
-        '<h2>Demande prise en compte</h2>
-        <p>Titre : ' . $sexe . '.</p>
-        <p>Prénom : ' .  $prenom . '.</p>
-        <p>Nom : ' . $nom . '.</p>
-        <p>E-mail : ' . $email . '.</p>
-        <p>' . $remember .'.</p>';
-
-        $message2 = 'Demande prise en compte
-        Titre : ' . $sexe . '
-        Prénom : ' . $prenom . '
-        Nom : ' . $nom . '
-        E-mail : ' . $email . '
-        ' . $remember .'.';
-
-    echo $message2;
-
-          mail('talla.m.seck@gmail.com','formulaire',$message)
+echo $sexe;
+echo $prenom;
+echo $nom;
+echo $email;
+       ?>
 
 
 
-?>
 </body>
 </html>

dfe4a733572658580b43dbfe5221ff8db9b588ce
wheeemyse <wheemyse>
2018-05-29 17:32:02 +0200

    final push

--- Exoparis/formulaire.php
+++ Exoparis/formulaire.php
@@ -9,23 +9,49 @@
 
 <?php
         $sexe = $_POST["sexe"];
-        $prenom =  $_POST["prenom"];
+        $prenom = $_POST["prenom"];
         $nom = $_POST["nom"];
-        $email = $_POST["mail"];
+        $email = $_POST["email"];
 
     if (isset($_POST["remember"])) {
      $remember = "se souvenir de moi";
+     $souvenir = 1;
     }
 
     else {
       $remember = 'ne pas se souvenir de moi';
+      $souvenir = 0;
     }
 
-echo $sexe;
-echo $prenom;
-echo $nom;
-echo $email;
-       ?>
+  echo $sexe;
+  echo $prenom;
+  echo $nom;
+  echo $email;
+
+$link = mysqli_connect("localhost","root","","Formulaire");
+
+if($link === false){
+  die("Connection failed: " . mysqli_connect_error());
+}
+$sql= "INSERT INTO Formulaire(Titre, Prenom, Nom, Email, Souvenir, Dateinsert) VALUES
+('$sexe','$prenom','$nom','$email','$souvenir',NOW())";
+
+if(mysqli_query($link,$sql)){
+echo "good";
+}else {
+  echo "bad" . mysqli_error($link);
+}
+
+if($email === SELECT * FROM Formulaire){
+  echo "Doublons";
+}
+else {
+  echo "Unique";
+}
+
+mysqli_close($link);
+
+?>
 
 
 
